32aca29921bcd201aa5420213ab057b599a3f60f
StephAdongo <stephanie@kabarak.ac.ke>
2024-05-22 10:31:22 +0300

    add sketch.js

--- sketch.js
+++ sketch.js
@@ -16,6 +16,27 @@ const hoover = document.createElement("hover");
 hoover.classList.add("hoover");
 
 
+const button = document.querySelector(".button");
 
+ function createGrid(gridSize) {
+   container.innerHTML = "";
+   const squareSize = Math.floor(container.clientWidth / gridSize);
+   for (let i = 0;i < gridSize * gridSize; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
+    container.appendChild(square);
+   }
+}
+
+button.addEventListener("click", () => {
+    let gridSize = prompt ("Enter number of squares per side (Max :100):");
+    while (gridSize === null || isNaN(gridSize) || gridSize > 100) {
+        gridSize = prompt("Invalid input. Please enter a number between 1 and 100:");
+      };
+    gridSize = parseInt(gridSize);
+    createGrid(gridSize);
+});
 
 
